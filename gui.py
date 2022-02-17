df192458a7f333e7d3e8ce030580262a9ed6b840
JackLynam <jack.lynam.19@cnu.edu>
2022-02-16 22:58:17 -0500

    Updated gui and refactored solution

--- gui.py
+++ gui.py
@@ -1,1 +1,102 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+from solution import *
+import math
+
+COLOR_DICT = {0:"gray", 1:"yellow", 2:"yellow", 3:"blue", 4:"blue", 5:"purple", 6:"purple", 7:"green", 8:"green", 9:"red", 10:"red"}
+
+class Piece():
+
+    def __init__(self, number, x, y):
+        self.number = number
+        self.x0 = x
+        self.y0 = y
+        if self.number % 2 == 0:
+            self.shape = 'r'
+        else:
+            self.shape = 't'
+        # TODO
+        self.shape = 'r'
+        self.color = COLOR_DICT[self.number] 
+        self.size = 35
+        self.text_offset = 18
+
+
+    def draw(self, canvas):
+        if self.shape == 'r':
+            x1, y1 = self.x0 + self.size, self.y0 + self.size
+            canvas.create_rectangle(self.x0, self.y0, x1, y1, fill=self.color, outline="black")
+            canvas.create_text(self.x0 + self.text_offset, self.y0 + self.text_offset, text=str(self.number), fill="black", font=("Helvetica 10 bold"))
+
+
+def left_cw():
+    pass
+
+def right_cw():
+    pass
+
+def left_ccw():
+    pass
+
+def right_ccw():
+    pass
+
+def solve():
+    pass
+
+def deg_to_rad(deg):
+    return deg * math.pi / 180
+
+def draw_state(canvas, state):
+    # state will be list of numbers, then we use this function to create Piece objects, then we finally draw the pieces
+    r = 100
+    x0_offset = 250
+    x1_offset = x0_offset + 1.7 * r
+    y_offset = 250
+
+    pieces = []
+    state_index = 0
+    thetas_left = range(60, 420, 30)
+    for theta in thetas_left:
+        x0, y0 = x0_offset + r*math.cos(deg_to_rad(theta)), y_offset + r*math.sin(deg_to_rad(theta)) 
+        temp_piece = Piece(state[state_index], x0, y0)
+        pieces.append(temp_piece)
+        state_index += 1
+    
+    thetas_right = range(-240, -510, -30)
+    for theta in thetas_right:
+        x0, y0 = x1_offset + r*math.cos(deg_to_rad(theta)), y_offset + r*math.sin(deg_to_rad(theta)) 
+        temp_piece = Piece(state[state_index], x0, y0)
+        pieces.append(temp_piece)
+        state_index += 1
+
+    for piece in pieces:
+        piece.draw(canvas)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('Two Wheel Puzzle Gui')
+    geometry = '900x700+50+50'
+    root.geometry(geometry)
+
+    button_frame = ttk.Frame(root)
+    button_frame.pack(side=tk.RIGHT)
+
+    button1 = ttk.Button(button_frame, text='1', command=left_cw)
+    button2 = ttk.Button(button_frame, text='2', command=right_cw)
+    button3 = ttk.Button(button_frame, text='3', command=left_ccw)
+    button4 = ttk.Button(button_frame, text='4', command=right_ccw)
+    buttonsolve = ttk.Button(button_frame, text='Solve', command=solve)
+    
+    PAD = 8
+    button1.pack(side=tk.TOP, ipadx=PAD, ipady=PAD)
+    button2.pack(side=tk.TOP, ipadx=PAD, ipady=PAD)
+    button3.pack(side=tk.TOP, ipadx=PAD, ipady=PAD)
+    button4.pack(side=tk.TOP, ipadx=PAD, ipady=PAD)
+    buttonsolve.pack(side=tk.TOP, ipadx=PAD, ipady=PAD)
+
+    canvas = tk.Canvas(root, width=700, height=500)
+    draw_state(canvas, INITIAL_STATE)
+    canvas.pack()
+    
+    root.mainloop()
--- solution.py
+++ solution.py
@@ -1,63 +1,46 @@
-class Problem:
+INITIAL_STATE = [0, 3, 4, 3, 0, 5, 6, 5, 0, 1, 2, 1, 0, 7, 8, 7, 0, 9, 10, 9, 0, 1, 2, 1]
+
+def is_goal_state(state):
     """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in this_ojb-oriented terminology: an abstract class).
+    state: Search state
 
-    You do not need to change anything in this class, EVER.
+    Returns True if and only if the state is a valid goal state.
     """
-    def __init__(self, state):
-        self.state = state
+    return state == INITIAL_STATE
 
 
-    def get_state(self):
-        """
-        Returns the start state for the search problem.
-        """
-        return self.state
+def get_successors(state):
+    """
+      state: Search state
 
-    def is_goal_state(self, state):
-        """
-          state: Search state
+    For a given state, this should return a list of triples, (successor,
+    action, stepCost), where 'successor' is a successor to the current
+    state, 'action' is the action required to get there, and 'stepCost' is
+    the incremental cost of expanding to that successor.
+    """
+    # left CW
+    state_1 = state[10:12] + state[0:10] + state[12:21] + state[7:10]
+    # print(state_1)
+    # right CW
+    state_2 = state[0:9] + state[11:23] + state[11:14]
+    # print(state_2)
+    # left CCW
+    state_3 = state[2:12] + state [0:2] + state[12:21] + state[11:12] + state[0:2]
+    # print(state_3)
+    # right CCW
+    state_4 = state[0:9] + state[19:21] + state[9:22]
+    # print(state_4)
 
-        Returns True if and only if the state is a valid goal state.
-        """
-        goal = [0, 3, 4, 3, 0, 5, 6, 5, 0, 1, 2, 1, 0, 7, 8, 7, 0, 9, 10, 9, 0, 1, 2, 1]
-        return state == goal
+    return [(state_1, 1, 1), (state_2, 2, 1), (state_3, 3, 1), (state_4, 4, 1)]
 
+def get_cost_of_actions(actions):
+    """
+     actions: A list of actions to take
 
-    # TODO
-    def get_successors(self, state):
-        """
-          state: Search state
-
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """
-        # left CW
-        state_1 = state[10:12] + state[0:10] + state[12:21] + state[7:10]
-        # print(state_1)
-        # right CW
-        state_2 = state[0:9] + state[11:23] + state[11:14]
-        # print(state_2)
-        # left CCW
-        state_3 = state[2:12] + state [0:2] + state[12:21] + state[11:12] + state[0:2]
-        # print(state_3)
-        # right CCW
-        state_4 = state[0:9] + state[19:21] + state[9:22]
-        # print(state_4)
-
-        return [(state_1, 1, 1), (state_2, 2, 1), (state_3, 3, 1), (state_4, 4, 1)]
-
-    def get_cost_of_actions(self, actions):
-        """
-         actions: A list of actions to take
-
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        return len(actions)
+    This method returns the total cost of a particular sequence of actions.
+    The sequence must be composed of legal moves.
+    """
+    return len(actions)
 
 
 class Queue:
@@ -89,47 +72,37 @@ def flatten(state):
          full_string += s
     return int(full_string)
 
-def breadth_first_search(problem):
-    """Search the shallowest nodes in the search tree first."""
-
-
+def breadth_first_search(init_state):
     reached = set({})
     
     frontier = Queue()
-    frontier.push((problem.get_state(), [], 1))
+    frontier.push((init_state, [], 1))
 
     while not frontier.is_empty():
-        state, path, curr_cost = frontier.pop()
-        if problem.is_goal_state(state):
+        state, path, _ = frontier.pop()
+        if is_goal_state(state):
             return path
 
-        if not flatten(state) in reached:
+        if get_cost_of_actions(path) < 16 and not flatten(state) in reached:
             reached.add(flatten(state))
-            for successor_state, action, cost in problem.get_successors(state):
+            for successor_state, action, cost in get_successors(state):
                 new_path = path[:]
                 new_path.append(action)
-                frontier.push((successor_state, new_path, curr_cost + cost))
+                frontier.push((successor_state, new_path, cost))
 
     print("FAILURE: Goal state not found")
 
 
 
 if __name__ == "__main__":
-#    num_cases = int(input("Input number of cases.\n"))
-#    for i in range(num_cases):
-#        start_config = input("Input starting configuration.\n")
-#        start_config = [int(x) for x in start_config.split(" ")]
-#
     start_config = input("Input starting configuration.\n")
     start_config = [int(x) for x in start_config.split(" ")]
-    # print(start_config)
-    problem = Problem(start_config)
-    solution = breadth_first_search(problem)
+    solution = breadth_first_search(start_config)
     if solution is None:
         print("Cannot solve with given arrangement")
     else:
         print("Solution path:")
         print(solution)
-        print("Length of solution path =", problem.get_cost_of_actions(solution))
+        print("Length of solution path =", get_cost_of_actions(solution))
     
 
